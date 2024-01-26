515a26d6fc02a19e378bde07f11300af58ab9360
SIST <SIST@DESKTOP-5G69MRI>
2024-01-26 15:38:19 +0900

    2024-01-19

---
+++ SpringDITotalProject/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,18 @@
+package com.sist.main;
+/*
+ *   1. 스프링에서 제공하는 컨테이너(클래스관리)
+ *        => 생명주기: 객체생성 => 객체 사용 => 객체 소멸 => destroy()
+ *                     |        | DL(getBean())
+ *             필요한 데이터가 있는 경우 => DI
+ *        => 스프링: 결합성이 낮은 프로그램
+ *                 *** 공통기반은 처리
+ *                 *** 핵심 기능만 소스 코딩
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
