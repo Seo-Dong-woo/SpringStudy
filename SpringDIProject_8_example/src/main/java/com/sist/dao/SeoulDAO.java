3572f804c453993cf2ac842bf9ad170f84844664
SIST <SIST@DESKTOP-5G69MRI>
2024-01-26 15:34:50 +0900

    2024-01-16

---
+++ SpringDIProject_8_example/src/main/java/com/sist/dao/SeoulDAO.java
@@ -0,0 +1,26 @@
+package com.sist.dao;
+
+import java.util.List;
+
+public class SeoulDAO {
+	private SeoulMapper mapper;
+
+	public void setMapper(SeoulMapper mapper) {
+		this.mapper = mapper;
+	}
+	
+	public List<SeoulVO> shopListData()
+	{
+		return mapper.shopListData();
+	}
+	
+	public SeoulVO shopDetailData(int no)
+	{
+		return mapper.shopDetailData(no);
+	}
+	
+	public List<SeoulVO> shopFindData(String title)
+	{
+		return mapper.shopFindData(title);
+	}
+}
