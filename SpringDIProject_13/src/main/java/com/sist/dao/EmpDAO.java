7c57211713072265323e0db43d67d83f528bbeaa
SIST <SIST@DESKTOP-5G69MRI>
2024-01-26 15:36:43 +0900

    2024-01-18

---
+++ SpringDIProject_13/src/main/java/com/sist/dao/EmpDAO.java
@@ -0,0 +1,20 @@
+package com.sist.dao;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.sist.mapper.*;
+
+@Repository // EmpDAO => empDAO(default)
+public class EmpDAO {
+	// mapper를 구현한 클래스 주소를 자동 주입
+	@Autowired
+	private EmpMapper mapper;
+	
+	public List<EmpVO> empListData()
+	{
+		return mapper.empListData();
+	}
+}
---
+++ SpringDIProject_13/src/main/java/com/sist/mapper/EmpMapper.java
@@ -0,0 +1,13 @@
+package com.sist.mapper;
+
+import java.util.*;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.sist.dao.*;
+
+public interface EmpMapper {
+	@Select("SELECT empno, ename, job, hiredate, TO_CHAR(hiredate, 'YYYY-MM-DD') as dbday, sal, deptno "
+			+ "FROM emp")
+	public List<EmpVO> empListData();
+}
