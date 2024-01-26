c154ba73ab3bf1822645638f7c1b2d5750e8972b
SIST <SIST@DESKTOP-5G69MRI>
2024-01-26 15:33:32 +0900

    2024-01-16

---
+++ SpringDIProject_3/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,26 @@
+package com.sist.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sist.spring.*;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// String[] xml= {"application-board.xml", "application-notice.xml"};
+		ApplicationContext app=new ClassPathXmlApplicationContext("application-*.xml");
+		
+		Board b=app.getBean("board", Board.class);
+		System.out.println("번호: " + b.getNo());
+		System.out.println("이름: " + b.getName());
+		System.out.println("제목: " + b.getSubject());
+		
+		Notice n=app.getBean("notice", Notice.class);
+		System.out.println("번호: " + n.getNo());
+		System.out.println("이름: " + n.getName());
+		System.out.println("제목: " + n.getContent());
+	}
+
+}
